fa5f7a9f5fec1af46ea504f879d064acf61bb1af
coldrain96 <Justtryagain961@gmail.com>
2020-04-28 01:45:06 +0500

    added features and mobile sizes display

--- src/components/DrawerComponent.vue
+++ src/components/DrawerComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="overlay">
-    <aside class="drawer">
+    <aside class="drawer" :class="{ 'bg-white': drawerActive }">
       <div class="drawer-hamburger hamburger">
         <a href="#" :class="{ activeNav: drawerActive }"
            @click.prevent="drawerActive = !drawerActive"
@@ -10,7 +10,11 @@
           <div class="hamburger-line line-3"></div>
         </a>
       </div>
-      <ul v-if="drawerActive" class="drawer__list">
+      <ul v-if="drawerActive"
+          class="drawer__list"
+          @click.prevent="drawerActive = !drawerActive"
+      >
+        <div class="nav-items-wrapper">
         <router-link
           v-for="route in $router.options.routes"
           :key="route.path"
@@ -22,6 +26,7 @@
         >
           <a class="nav-link">{{ route.name }}</a>
         </router-link>
+        </div>
       </ul>
     </aside>
   </div>
@@ -33,6 +38,32 @@
       drawerActive: false,
       overlay: false,
     }),
+    methods: {
+      switchCurrentRoute() {
+        const routesArray = []
+        this.$router.options.routes.forEach((item) => {
+          routesArray.push(item.path)
+        })
+      }
+    }
   }
 </script>
 <style lang="scss" src="@/assets/scss/components/drawer.scss" scoped></style>
+<style lang="scss" scoped>
+  @media only screen and (max-width: 991px), only screen and (max-device-width: 991px) {
+    .drawer {
+      width: 100%;
+      height: 60px;
+      background: none;
+    }
+    .drawer__list {
+      background-color: #fff;
+    }
+    .mob-page-title {
+      padding-top: 72px !important;
+    }
+    .nav-items-wrapper {
+      height: 100vh;
+    }
+  }
+</style>
