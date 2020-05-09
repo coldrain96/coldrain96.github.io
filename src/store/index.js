fa963c09658413beee5b8a17c11b64af987f0e7a
coldrain96 <Justtryagain961@gmail.com>
2020-05-09 17:48:36 +0500

    deleted tools module

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,6 @@ import about from './about/'
 import contacts from './contacts/'
 import facts from './facts/'
 import projects from './projects/'
-import tools from './tools/'
 
 Vue.use(Vuex)
 
@@ -13,7 +12,6 @@ const modules = {
   contacts,
   facts,
   projects,
-  tools,
 }
 
 export default new Vuex.Store({
