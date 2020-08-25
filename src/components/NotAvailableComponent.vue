1ec9a306dd94bf63637850596dc3b347659a9e16
Kamil Bikmetov <justtryagain961@gmail.com>
2020-08-25 14:03:25 +0300

    delete unnecessary components and styles

--- src/components/NotAvailableComponent.vue
+++
@@ -1,4 +0,0 @@
-<template>
-	<div class="mobile"><strong>Sorry but mobile version it's not available yet.</strong></div>
-</template>
-<style lang="scss" src="@/assets/scss/components/not-available.scss" scoped></style>
